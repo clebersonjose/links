fb69dad1753be754f0f8925406e5c248727fa10a
clebersonjose <clebersonjosea@gmail.com>
2024-07-27 20:10:07 -0300

    feat(perfil): adicinando retorno dos dados de perfil na api

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PerfilController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () =>'welcome');
+Route::get('perfis', [PerfilController::class, 'index']);
+Route::get('perfil/{id}', [PerfilController::class, 'show']);
+Route::get('perfil', [PerfilController::class, 'showLast']);
