712dcf9771e0e0f0ed1224ae14a3a4917d4df4b5
clebersonjose <clebersonjosea@gmail.com>
2024-08-08 21:08:24 -0300

    Add logica e rotas de Redes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\PerfilController;
+use App\Http\Controllers\RedeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('perfis', [PerfilController::class, 'index']);
 Route::get('perfil/{id}', [PerfilController::class, 'show']);
 Route::get('perfil', [PerfilController::class, 'showLast']);
+
+Route::get('redes', [RedeController::class, 'index']);
